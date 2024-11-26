4c08aa3543076d60372ef18e189072c722f46b04
Santiago <svilla0918@gmail.com>
2024-11-25 22:32:04 -0600

    Publicar artículo funcionando correctamente

--- views/javascript/funcionamiento_botones.js
+++ views/javascript/funcionamiento_botones.js
@@ -1,25 +1,58 @@
+const newArticleButton = document.getElementById('newArticleButton');
 const publicarButton = document.getElementById('publicarButton');
 const articleContent = document.getElementById('articleContent');
+const articleTitle = document.getElementById('articleTitle');
 
-publicarButton.onclick = () => {
-    // const xhr = new XMLHttpRequest();
+newArticleButton.onclick = () => {
+    const url = "http://localhost:3000/users";
+    const xhr = new XMLHttpRequest();
 
-    const url = "http://localhost:3000/posts";
-
-
-    const content = JSON.stringify(articleContent.value);
-
-    console.log(content);
-
-    xhr.open('POST', url);
+    xhr.open('GET', url);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(content);
-
+    xhr.send();
     xhr.onload = () => {
-        if (xhr.status !== 200) {
-            console.log(xhr.statusText + ": " + xhr.status);
+        if(xhr.status !== 200) {
+            console.log(xhr.statusText + ': ' + xhr.status);
         } else {
+            console.log("La respuesta del servidor es: ")
             console.log(xhr.responseText);
+            const user = JSON.parse(xhr.responseText);
+            console.log("La informacion del usuario es: ")
+            console.log(user);
+
+            publicarButton.onclick = () => {
+                const xhrPublicar = new XMLHttpRequest();
+                
+                const url = "http://localhost:3000/posts";
+                
+                const information = {
+                    userId: user[0]._id,
+                    date: new Date(),
+                    likes: 0,
+                    title: articleTitle.value,
+                    content: articleContent.value
+                };
+            
+                const publicar = JSON.stringify(information);
+            
+                console.log("Lo que se va a publicar es:")
+                console.log(publicar);
+            
+                xhrPublicar.open('POST', url);
+                xhrPublicar.setRequestHeader('Content-Type', 'application/json');
+                xhrPublicar.send(publicar);
+            
+                xhrPublicar.onload = () => {
+                    if (xhrPublicar.status !== 200) {
+                        console.log(xhrPublicar.statusText + ": " + xhrPublicar.status);
+                    } else {
+                        console.log('Artículo publicado exitosamente!');
+                    }
+                }
+            }
         }
     }
+
+
 }
+
